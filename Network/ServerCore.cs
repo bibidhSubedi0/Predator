0bd1fb0fa2377479b372de4e00c63551ea1b5534
bibidhSubedi0 <080bct021.bibidh@pcampus.edu.np>
2025-03-13 02:04:06 +0545

    Setting up for a new Network Module

---
+++ Network/ServerCore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+class GameServer
+{
+    private TcpListener listener;
+    private bool isRunning = false;
+
+    public GameServer()
+    {
+        listener = new TcpListener(IPAddress.Any, 5000);
+    }
+
+    public void Start()
+    {
+        isRunning = true;
+        listener.Start();
+        Console.WriteLine("Server started. Waiting for players...");
+
+        while (isRunning)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Player connected!");
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    private void HandleClient(TcpClient client)
+    {
+        Console.WriteLine("Handling new player...");
+    }
+}
